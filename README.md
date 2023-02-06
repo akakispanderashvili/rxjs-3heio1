b9b1374d28a46212f506005d7f830bbfce307ae2
akakispanderashvili <86373282+akakispanderashvili@users.noreply.github.com>
2023-02-07 03:35:52 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-3heio1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-3heio1)
---
+++ index.ts
@@ -0,0 +1,67 @@
+import {
+  from,
+  Observable,
+  of,
+  timer,
+  takeUntil,
+  mergeMap,
+  filter,
+  tap,
+  scan,
+  reduce,
+  map,
+} from 'rxjs';
+
+// 1))
+//
+//
+//
+const people1 = [
+  { name: 'John', age: 30 },
+  { name: 'Tom', age: 25 },
+  { name: 'Jim', age: 35 },
+];
+const people2 = [
+  { name: 'Jill', age: 28 },
+  { name: 'Jack', age: 32 },
+  { name: 'Kate', age: 40 },
+];
+
+const obs$ = from([people1, people2]).pipe(
+  mergeMap((people) =>
+    from(people).pipe(
+      filter((person) => person.name[0] === 'J'),
+      reduce((totalAge, person) => totalAge + person.age, 0)
+    )
+  )
+);
+
+obs$.subscribe((x) => console.log(x));
+
+// 2)))
+//
+//
+
+const numbers = [1, 2, 3, 4, 5];
+const stopSignal = timer(3000);
+
+of(...numbers)
+  .pipe(takeUntil(stopSignal))
+  .subscribe(
+    (x) => console.log(`emitted number: ${x}`),
+    (error) => console.error(error),
+    () => console.log('completed')
+  );
+
+// 3)))
+//
+//
+
+const newNumbers = [1, 2, 3, 4, 5];
+
+const secondObs$ = from(newNumbers).pipe(
+  tap((x) => console.log(`Emitting number: ${x}`)),
+  scan((acc, x) => acc + x)
+);
+
+// secondObs$.subscribe((sum) => console.log(`The sum is: ${sum}`));
